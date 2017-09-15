578a08e46b58dc2336bd2c29e9f6657f41d265b7
elopez01 <catracho4lf@yahoo.com>
2017-09-15 14:02:17 -0400

    Added Modified file
    Updated file to fix formatting and added comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,52 +1,103 @@
 import fresh_tomatoes
 import media
 
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
+
+
 don_juan = media.Movie("Don Juan DeMarco",
-                        "Have you ever loved a woman?",
-                        "https://upload.wikimedia.org/wikipedia/en/b/b8/Don_juan_demarco.jpg",
-                        "https://www.youtube.com/watch?v=MCpLSvW5s-A",
-                        "https://en.wikipedia.org/wiki/Don_Juan_DeMarco",
-                        1994)
+    "Have you ever loved a woman?",
+    "https://upload.wikimedia.org/wikipedia/en/b/b8/Don_juan_demarco.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=MCpLSvW5s-A",
+    "https://en.wikipedia.org/wiki/Don_Juan_DeMarco",  # NOQA
+    1994
+    )
 
-
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
 beauty_and_beast = media.Movie("Beauty and the Beast",
-                               "A tale about love",
-                               "https://upload.wikimedia.org/wikipedia/en/7/7c/Beautybeastposter.jpg",
-                               "https://www.youtube.com/watch?v=dGboC3UVvX8",
-                               "https://en.wikipedia.org/wiki/Beauty_and_the_Beast_(1991_film)",
-                               1991)
+   "A tale about love",
+   "https://upload.wikimedia.org/wikipedia/en/7/7c/Beautybeastposter.jpg",  # NOQA
+   "https://www.youtube.com/watch?v=dGboC3UVvX8",
+   "https://en.wikipedia.org/wiki/Beauty_and_the_Beast_(1991_film)",  # NOQA
+   1991
+   )
 
 
-
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
 dark_knight = media.Movie("Dark Knight",
-                          "Batman goes against the Joker",
-                          "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
-                          "https://www.youtube.com/watch?v=EXeTwQWrcwY",
-                          "https://en.wikipedia.org/wiki/The_Dark_Knight_(film)",
-                          2008)
+  "Batman goes against the Joker",
+  "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",  # NOQA
+  "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+  "https://en.wikipedia.org/wiki/The_Dark_Knight_(film)",  # NOQA
+  2008
+  )
 
-
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
 the_waterboy = media.Movie("The Waterboy",
-                           "Foosball is the Devil!",
-                           "https://upload.wikimedia.org/wikipedia/en/f/f3/Waterboy-poster-0.jpg",
-                           "https://www.youtube.com/watch?v=z8yv9eq5s14",
-                           "https://en.wikipedia.org/wiki/The_Waterboy",
-                           1998)
+   "Foosball is the Devil!",
+   "https://upload.wikimedia.org/wikipedia/en/f/f3/Waterboy-poster-0.jpg",  # NOQA
+   "https://www.youtube.com/watch?v=z8yv9eq5s14",
+   "https://en.wikipedia.org/wiki/The_Waterboy",
+   1998
+   )
 
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
 scarface = media.Movie("Scarface",
-                       "The world is yours and everything in it!",
-                       "https://upload.wikimedia.org/wikipedia/en/7/71/Scarface_-_1983_film.jpg",
-                       "https://www.youtube.com/watch?v=7pQQHnqBa2E",
-                       "https://en.wikipedia.org/wiki/Scarface_(1983_film)",
-                       1983)
+   "The world is yours and everything in it!",
+   "https://upload.wikimedia.org/wikipedia/en/7/71/Scarface_-_1983_film.jpg",  # NOQA
+   "https://www.youtube.com/watch?v=7pQQHnqBa2E",
+   "https://en.wikipedia.org/wiki/Scarface_(1983_film)",  # NOQA
+   1983
+   )
 
 
+'''
+Adding information about the movie:
+movie_title, movie_storyline, poster_image,
+movie_trailer_url, movie_information, release_date
+'''
 independence_day = media.Movie("Independence Day",
-                               "Going back in time to meeet the authors",
-                               "https://upload.wikimedia.org/wikipedia/en/b/bb/Independence_day_movieposter.jpg",
-                               "https://www.youtube.com/watch?v=kA2WzBi2grE",
-                               "https://en.wikipedia.org/wiki/Independence_Day_(1996_film)",
-                               1996)
+   "Going back in time to meeet the authors",
+   "https://upload.wikimedia.org/wikipedia/en/b/bb/Independence_day_movieposter.jpg",  # NOQA
+   "https://www.youtube.com/watch?v=kA2WzBi2grE",
+   "https://en.wikipedia.org/wiki/Independence_Day_(1996_film)",  # NOQA
+   1996
+   )
 
-movies = [don_juan, beauty_and_beast, dark_knight, the_waterboy, scarface, independence_day]
+'''
+Adding Movies to array to be called by the
+fresh_tomatoes.open_movies_page method
+'''
+movies = [
+    don_juan,
+    beauty_and_beast,
+    dark_knight,
+    the_waterboy,
+    scarface,
+    independence_day
+    ]
+
+'''
+Passing the movies array as a parameter to
+open_movies_page function
+'''
 fresh_tomatoes.open_movies_page(movies)
